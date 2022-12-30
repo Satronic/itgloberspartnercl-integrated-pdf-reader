2c7223eaa5662f6616cb012ea0f25e8e75662196
Sneider <electronsgm@gmail.com>
2022-12-30 18:05:54 -0500

    FEAT(/react): Add props types and objetc element for to render pdf file

--- react/components/PdfReader.tsx
+++ react/components/PdfReader.tsx
@@ -1,8 +1,33 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function PdfReader() {
-  return <div>PDF Reader</div>
+type PdfReaderProps = {
+  pdfUrl: string
+  width: number
+  height: number
+}
+
+const PdfReader = ({ pdfUrl, width, height }: PdfReaderProps) => {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    mounted && (
+      <div className="flex justify-center">
+        <object
+          data={pdfUrl}
+          type="application/pdf"
+          width={width}
+          height={height}
+        >
+          Este navegador no soporta archivos PDF
+        </object>
+      </div>
+    )
+  )
 }
 
 export default PdfReader
